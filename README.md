0f4004bd2c6c60827f8f8c60be5ca58d6dcef264
Hong Xu <hong@topbug.net>
2020-12-20 21:13:37 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# poorconn
+
+Simulating poor network connections.
+
+To start developing, install all development packages:
+
+    pip install -r requirements-dev.txt
+
+To run lint, run:
+
+    tox -e lint
+
+To run tests, run:
+
+    tox -e py36  # or py37, py38, py39, whichever you are using
---
+++ tests/test_version.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2020  Hong Xu <hong@topbug.net>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import packaging.version
+
+import poorconn
+
+
+def test_version():
+    "Test version string."
+
+    # this line must not raise packaging.version.InvalidVersion
+    packaging.version.Version(poorconn.__version__)
+    assert poorconn.__version__ == poorconn._version.version
